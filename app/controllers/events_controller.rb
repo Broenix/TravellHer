a205179ef76137a9ab54ca1e85c9149facd9b680
Paul Barbaud <paolbarbaud@gmail.com>
2022-11-29 12:18:15 +0100

    index and show done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,12 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: :show
+
   def index
     @events = Event.all
+  end
+
+  def show
+    @events = Event.find(params[:id])
   end
 end
