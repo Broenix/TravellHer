6e64b65096577d51f488aa1aa28069aafdc8bede
Paul Barbaud <paolbarbaud@gmail.com>
2022-12-06 10:09:36 +0100

    pull updates updates

--- db/schema.rb
+++ db/schema.rb
@@ -105,6 +105,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_161210) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.float "rating"
     t.index ["category_id"], name: "index_spots_on_category_id"
   end
 
