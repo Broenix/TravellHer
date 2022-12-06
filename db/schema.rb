4ccfea2fd108357eeb720cc712563a50bd33a4b8
Chloe Lagneaux <chloelagneaux2@gmail.com>
2022-12-06 15:38:24 +0100

    404 + 500

--- db/schema.rb
+++ db/schema.rb
@@ -106,7 +106,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_091852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
-    t.float "rating"
     t.index ["category_id"], name: "index_spots_on_category_id"
   end
 
